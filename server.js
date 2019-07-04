ebee8136fb60123199670b0bfc94c8fd267990b8
brandonngiam <brandonngiam@user.noreply.github.com>
2019-07-04 21:43:03 +0800

    Added environment variable for production server

--- server.js
+++ server.js
@@ -1,4 +1,10 @@
 const app = require("./src/app");
 const port = process.env.PORT || 3001;
 
-app.listen(port, () => console.log(`Listening on port ${port}`));
+app.listen(port, () => {
+  if (process.env.NODE_ENV === "production") {
+    console.log(`Server is running on Heroku with port ${port}`);
+  } else {
+    console.log(`Server is running on http://localhost:${port}`);
+  }
+});
