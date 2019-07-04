29c01444ca8cebf98067e1a70c24510d5b0dedc0
brandonngiam <brandonngiam@user.noreply.github.com>
2019-07-04 22:08:13 +0800

    Added environment variable for mongoDB production server

--- src/util/db.js
+++ src/util/db.js
@@ -2,12 +2,19 @@ const mongoose = require("mongoose");
 
 //must add scenario for production
 //we have three databases: local, production, in-memory for tests
-const dbURL = global.__MONGO_URI__ || "mongodb://localhost:27017/twitta";
+const dbURL =
+  process.env.NODE_ENV === "production"
+    ? process.env.DATABASE_URL
+    : global.__MONGO_URI__ || "mongodb://localhost:27017/twitta";
 mongoose.connect(dbURL, { useNewUrlParser: true });
 mongoose.set("useFindAndModify", false);
 const db = mongoose.connection;
 
 db.on("error", console.error.bind(console, "Mongo connection error:"));
 db.once("open", () => {
-  console.log(`Connection to mongoDB on ${dbURL}`);
+  if (process.env.NODE_ENV === "production") {
+    console.log(`Production: connection to mongoDB on ${dbURL}`);
+  } else {
+    console.log(`Development: connection to mongoDB on ${dbURL}`);
+  }
 });
